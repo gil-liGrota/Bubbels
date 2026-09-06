5121246841db65ef33826aef41abcb9efc2c5fb9
Your Name <you@mail.com>
2026-09-06 14:49:37 +0300

    i think 1 is working not sure about that

--- Bubble.py
+++ Bubble.py
@@ -85,6 +85,19 @@ def is_isolated_inner(bubbles_grid, bubble_location, locations_checked):
 # ---------------------------------your code-----------------------------------
 # -----------------------------------------------------------------------------
 
+# TODO: implement
 def should_stop(bubbles_grid, bullet_bubble):
-    # TODO: implement
-    pass
+    for row_in_grid in bubbles_grid:
+        for bubble in row_in_grid:
+            if bullet_bubble["color"] != consts.NO_BUBBLE:
+                if get_distance(bubble, bullet_bubble) <= consts.BUBBLE_RADIUS:
+                    return True
+
+
+    if consts.WINDOW_HEIGHT - consts.BUBBLE_RADIUS < bullet_bubble["center_y"]: #FIXME if its get to the top it wont stop
+        return True
+
+    return False
+
+def get_distance(bubble, bullet_bubble):
+    return math.sqrt((bubble["center_x"] - bullet_bubble["center_x"]) ** 2 + (bubble["center_y"] - bullet_bubble["center_y"]) ** 2)
